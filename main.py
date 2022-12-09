d147337cb484bb7b9fb6c96a5f4f496157875886
Nazar Marusyn <marusinnazar@gmail.com>
2022-12-09 13:12:50 +0200

    Update on pypi

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from cacao import cacao
+from cacao.src.cacao.cacao import classes
 
 
-class Marks(cacao.Schema):
-    mark_1 = cacao.IntegerField(min_value=0, max_value=100)
-    mark_2 = cacao.IntegerField(min_value=0, max_value=100)
+class Marks(classes.Schema):
+    mark_1 = classes.IntegerField(min_value=0, max_value=100)
+    mark_2 = classes.IntegerField(min_value=0, max_value=100)
 
 
-class Test(cacao.Schema):
-    full_name = cacao.StringField(max_length=200)
-    age = cacao.IntegerField(default=21, min_value=18, max_value=21)
-    money = cacao.DecimalField(write_only=True)
-    marks = cacao.NestedField(Marks)
+class Test(classes.Schema):
+    full_name = classes.StringField(max_length=200)
+    age = classes.IntegerField(default=21, min_value=18, max_value=21)
+    money = classes.DecimalField(write_only=True)
+    marks = classes.NestedField(Marks)
 
 
 marks = Marks(mark_1=86, mark_2=98)
