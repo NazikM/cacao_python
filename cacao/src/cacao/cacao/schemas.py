f72745ce69918e025dd5fac1ac6758fc22aaaa9f
Nazar Marusyn <marusinnazar@gmail.com>
2022-12-15 18:17:22 +0200

    A lot of upgrades

--- cacao/src/cacao/cacao/schemas.py
+++ cacao/src/cacao/cacao/schemas.py
@@ -1,25 +1,36 @@
 from cacao.src.cacao.cacao.fields import Field
 from cacao.src.cacao.cacao.missing import MISSING
 
-# __all__ = (
-#     'Schema',
-#     'MISSING',
-#     # 'Field',
-#     # 'IntegerField',
-#     # 'StringField',
-#     # 'DecimalField',
-#     # 'DateTimeField',
-#     # 'NestedField'
-# )
+
+class Foo(dict):
+    def __missing__(self, key):
+        def wrapper(*args, **kwargs):
+            self[key] = func(*args, **kwargs)
+            return self[key]
+        if not key.startswith('__'):
+            if key == "classmethod" or key in Field.Fields or key == "fields":
+                raise KeyError
+            key, value = self.separate_key(key)
+            func = Field.Fields[value]
+            return wrapper
+
+    def separate_key(self, key):
+        for field_name in Field.Fields:
+            if field_name in key:
+                return key.replace(field_name, ''), field_name
+        return key
 
 
 class OnlyFieldsMeta(type):
+    def __prepare__(self, *a):
+        return Foo()
+
     def __new__(cls, clsname, bases, attrs):
         for name, val in attrs.items():
             if name not in ('__module__', '__qualname__'):
                 if not isinstance(val, (Field, classmethod)) and not callable(val):
                     raise ValueError("You cannot create not Field attributes inside the Schema")
-        return type(clsname, bases, attrs)
+        return super().__new__(cls, clsname, bases, attrs)
 
 
 class Schema(metaclass=OnlyFieldsMeta):
@@ -28,9 +39,14 @@ class Schema(metaclass=OnlyFieldsMeta):
             if key not in vars(type(self)):
                 raise TypeError("Too many arguments!")
 
-        for key, value in vars(type(self)).items():
-            if isinstance(value, Field):
-                setattr(self, key, kwargs.get(key, MISSING))
+        # for key, value in vars(type(self)).items():
+        #     if isinstance(value, Field):
+        #         setattr(self, key, kwargs.get(key, MISSING))
+        # print(kwargs)
+        self.__post_init__(**kwargs)
+
+    def __post_init__(self, **kwargs):
+        ...
 
     @classmethod
     def from_dict(cls, data):
@@ -38,7 +54,7 @@ class Schema(metaclass=OnlyFieldsMeta):
 
     def to_dict(self):
         res = {}
-        for key, value in type(self).__dict__.items():
+        for key, value in vars(type(self)).items():
             if isinstance(value, Field) and not value.write_only:
                 val = getattr(self, key)
                 if val is not MISSING:
@@ -46,4 +62,3 @@ class Schema(metaclass=OnlyFieldsMeta):
                         val = val.to_dict()
                     res[key] = val
         return res
-
--- cacao/tests/main.py
+++ cacao/tests/main.py
@@ -1,38 +1,58 @@
 from cacao.src.cacao.cacao import fields, schemas
 
 
+class MyIntegerField(fields.IntegerField):
+    def __get__(self, obj, obj_type=None):
+        return super().__get__(obj, obj_type) + 100
+
+
 class Marks(schemas.Schema):
-    mark_1 = fields.IntegerField(min_value=0, max_value=100)
-    mark_2 = fields.IntegerField(min_value=0, max_value=100)
+    mark_1 = MyIntegerField(min_value=0, max_value=200)
+    mark_2IntegerField(min_value=0, max_value=200)
+
+    def __post_init__(self, mark_1, mark_2):
+        self.mark_1 = mark_1
+        self.mark_2 = mark_2
 
 
-class Test(schemas.Schema):
-    full_name = fields.StringField(max_length=200)
-    age = fields.IntegerField(default=21, min_value=18, max_value=27)
-    money = fields.DecimalField(write_only=True)
-    marks = fields.NestedField(Marks)
-    rich = fields.MethodField()
-
-    def get_rich(self):
-        return True if self.money > 26 else False
+marks2 = Marks(mark_1=6, mark_2=62)
+print(marks2.to_dict())
 
 
-marks = Marks(mark_1=86, mark_2=98)
-t = Test(full_name="Nazarii Marusyn", age='20', money='27.4564566', marks={"mark_1": 86, "mark_2": 98})
-t.age = 19
-print(t.to_dict())
 
-data = {
-    "full_name": "Nazarii Marusyn",
-    'age': 21,
-    'money': "33.34534",
-    "marks": {
-        "mark_1": 86,
-        "mark_2": 98
-    }
-}
-t2 = Test.from_dict(data)
-print(t2.to_dict())
+# marks = Marks(mark_1=96, mark_2=98, mark=6)
+
+# print(marks.to_dict())
+
+
+# class Test(schemas.Schema):
+#     full_name = fields.StringField(max_length=200)
+#     age = fields.IntegerField(default=21, min_value=18, max_value=27)
+#     money = fields.DecimalField(write_only=True)
+#     marks = fields.NestedField(Marks)
+#     rich = fields.MethodField()
+#
+#     def get_rich(self):
+#         return True if self.money > 26 else False
+
+
+
+# t = Test(full_name="Nazarii Marusyn", age='20', money='27.4564566', marks={"mark_1": 86, "mark_2": 98})
+# t.age = 19
+# print(t.to_dict())
+
+
+# data = {
+#     "full_name": "Nazarii Marusyn",
+#     'age': 21,
+#     'money': "33.34534",
+#     "marks": {
+#         "mark_1": 86,
+#         "mark_2": 98
+#     }
+# }
+# t2 = Test.from_dict(data)
+# print(t2.to_dict())
 
 
 class TestSchema(schemas.Schema):
@@ -40,11 +60,23 @@ class TestSchema(schemas.Schema):
     nested_schema = fields.NestedField('self')
 
 
-# data2 = {
-#     'name': 't',
-#     'nested_schema': {
-#         'name': 'world'
-#     }
-# }
-#
-# print(TestSchema.from_dict(data2).to_dict())
+data2 = {
+    'name': 'Hello',
+    'nested_schema': {
+        'name': 'world',
+        'nested_schema': {
+            'name': 'world'
+        }
+    }
+}
+
+# test2 = TestSchema.from_dict(data2)
+# print(test2.to_dict())
+
+
+# class Test2Schema(schemas.Schema):
+#     full_name = fields.StringField(max_length=200)
+#     testStringField(max_length=200)
+
+
+# print(vars(Test2Schema))
